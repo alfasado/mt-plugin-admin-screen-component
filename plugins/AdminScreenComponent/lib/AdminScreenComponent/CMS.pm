bd63ee2a12f06b5232e847b3fea95f222f293b92
Junnama Noda <junnama@alfasado.jp>
2014-07-29 15:14:03 +0900

    Added. Step1 to Step4.

--- plugins/AdminScreenComponent/lib/AdminScreenComponent/CMS.pm
+++ plugins/AdminScreenComponent/lib/AdminScreenComponent/CMS.pm
@@ -7,7 +7,16 @@ sub _app_cms_mode_admin_screen_component {
     my $app = shift;
     my $component = MT->component( 'AdminScreenComponent' );
     my %param;
-    my $tmpl = File::Spec->catfile( $component->path, 'tmpl', 'component.tmpl' );
+    my $tmpl_name = 'component';
+    if ( my $step = $app->param( 'step' ) ) {
+        if ( $step =~ /^[1-4]$/ ) {
+            $tmpl_name .= '_step_' . $step;
+        }
+    } else {
+        $tmpl_name .= '_step_4';
+    }
+    $param{ screen_group } = 'admin_screen_component'; 
+    my $tmpl = File::Spec->catfile( $component->path, 'tmpl', $tmpl_name . '.tmpl' );
     return $app->build_page( $tmpl, \%param );
 }
 
